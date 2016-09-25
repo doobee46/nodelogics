0d7f927a4517a8393072c5c57dbc257e7c471cc2
Obed lorisson <obed.lorisson@gmail.com>
2016-09-24 21:39:12 -0400

    add hosting page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( cloud-bg.png )
 Rails.application.config.assets.precompile += %w( smallservers.png )
+Rails.application.config.assets.precompile += %w( webhosting.png )

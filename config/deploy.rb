8ce2f97b011c443c29560de10603d588d00b1e3e
Obed lorisson <obed.lorisson@gmail.com>
2016-12-18 21:35:45 -0500

    add rake task to capistrano to seed database

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,11 @@ namespace :deploy do
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
+
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end

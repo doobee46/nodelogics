3055790f0902d45ba4d097a176117c9f7a0efdd9
Obed lorisson <obed.lorisson@gmail.com>
2017-01-02 17:49:42 -0500

    add geocoder gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
     posts_path
   end 
 
- def set_locale
+=begin
+  def set_locale
     if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
       l = cookies[:educator_locale].to_sym
     else
@@ -23,7 +24,33 @@ class ApplicationController < ActionController::Base
       cookies.permanent[:educator_locale] = l
     end
     I18n.locale = l
+=end
+
+def set_locale
+  if cookies[:educator_locale] && I18n.available_locales.include?(cookies[:educator_locale].to_sym)
+    l = cookies[:educator_locale].to_sym
+  else
+    begin
+      country_code = request.location.country_code
+      if country_code
+        country_code = country_code.downcase.to_sym
+        locale = I18n.available_locales
+          if locale.include?(country_code)
+            l = country_code
+          end
+         # use russian for CIS countries, english for others
+        #[:ru, :kz, :ua, :by, :tj, :uz, :md, :az, :am, :kg, :tm].include?(country_code) ? l = :ru : l = :en
+      else
+        l = I18n.default_locale # use default locale if cannot retrieve this info
+      end
+    rescue
+      l = I18n.default_locale
+    ensure
+      cookies.permanent[:educator_locale] = l
+    end
   end
+  I18n.locale = l
+end
  
 
   protected
